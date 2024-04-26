67a79ea5f5b5ae929ee4c7628c390eb7c47d2732
mac <mac@blockapi.dev>
2024-04-26 11:39:17 +0800

    promisified publishing msg to tcp

--- src/index.ts
+++ src/index.ts
@@ -7,113 +7,135 @@ const extraSpaceArray = new Array(4);
 const extraSpace = Buffer.from(extraSpaceArray);
 
 class HatiSdk {
-  private static options_: any;
+  private static options_: { host: string; port: number } = {
+    host: "localhost",
+    port: 4242,
+  };
 
-  constructor(options?: any) {
+  private static client_: Socket;
+
+  constructor(options?: { host: string; port: number }) {
     if (options) {
       HatiSdk.options_ = options;
+
+      HatiSdk.client_ = createConnection({
+        host: options.host,
+        port: options.port,
+      });
     }
   }
 
-  static setOptions(options: any): void {
+  static setOptions(options: { host: string; port: number }): void {
     HatiSdk.options_ = options;
+
+    HatiSdk.client_ = createConnection({
+      host: options.host,
+      port: options.port,
+    });
   }
 
   prepareMessage(payload: string): string {
-    // let preparedMessage = `${hatiHeader}${version}${extraSpace}CL:${payload.length}\n${payload}EOP\n--hati\n\r`;
-    let preparedMessage = `${payload}\n`;
-
-    return preparedMessage;
+    return `${payload}\n`;
   }
 
   async publish(message: string): Promise<any> {
-    const client = createConnection({
-      host: "localhost",
-      port: 4242,
-      // host: "roundhouse.proxy.rlwy.net",
-      // port: 11402,
-      // host: "monorail.proxy.rlwy.net",
-      // port: 17092,
-    });
-    // client.setNoDelay();
+    return new Promise((resolve, reject) => {
+      HatiSdk.client_.write(this.prepareMessage(message));
 
-    let okCount = 0;
-    let receivedCount = 0;
-
-    const maxCount = 1;
-    const startTime: any = new Date();
-
-    const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
-
-    for (let i = 0; i < maxCount; i++) {
-      await delay(1 / 200); // 1000 = 1s
-
-      // setImmediate(() => {
-      const msg = this.prepareMessage(message);
-      // run something
-      const ok = client.write(`${msg}`, (err) => {
-        if (err) {
-          console.error("error: ", err);
+      HatiSdk.client_.on("data", (data) => {
+        resolve(data.toString());
+        if (data.toString().endsWith("EXIT")) {
+          HatiSdk.client_.destroy();
         }
       });
 
-      if (ok) {
-        okCount++;
-      } else {
-        // client.pause();
-      }
-      // });
-    }
-
-    const endTime: any = new Date();
-    const timeElapsed: any = endTime - startTime;
-
-    console.log("timeElapsed: ", timeElapsed);
-    console.log("okCount: ", okCount);
-
-    client.on("data", function (data) {
-      if (data.toString() === "+OK\n") {
-        // console.log("ok");
-        return;
-      }
-
-      // var is_kernel_buffer_full = client.write(data);
-      // if (!is_kernel_buffer_full) {
-      //   client.pause();
-      // }
-      // if (is_kernel_buffer_full) {
-      //   console.log(
-      //     "Data was flushed successfully from kernel buffer i.e written successfully!"
-      //   );
-      // } else {
-      //   client.pause();
-      // }
-      // receivedCount++;
-      // client.destroy(); // kill client after server's response
-      // console.log("Received: " + data);
-      // if (maxCount === receivedCount) {
-      //   console.log("YESSSSSSSSS");
-      // } else {
-      //   console.log(receivedCount);
-      // }
+      HatiSdk.client_.on("error", (err) => {
+        reject(err);
+      });
     });
+    // let okCount = 0;
+    // let receivedCount = 0;
 
-    client.on("close", function () {
-      console.log("Connection closed");
-    });
+    // const maxCount = 100000;
+    // const startTime: any = new Date();
 
-    client.on("timeout", function () {
-      console.log("Socket timed out !");
-      client.end("Timed out!");
-      // can call socket.destroy() here too.
-    });
+    // const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
-    client.on("drain", function () {
-      console.log(
-        "write buffer is empty now .. u can resume the writable stream"
-      );
-      client.resume();
-    });
+    // for (let i = 0; i < maxCount; i++) {
+    //   await delay(1 / 600);
+
+    //   // setImmediate(() => {
+    //   const msg = this.prepareMessage(message);
+    //   // run something
+    //   const ok = client.write(`${msg}`, (err) => {
+    //     if (err) {
+    //       console.error("error: ", err);
+    //     }
+    //   });
+
+    //   if (ok) {
+    //     okCount++;
+    //   } else {
+    //     // client.pause();
+    //   }
+    //   // });
+    // }
+
+    // const endTime: any = new Date();
+    // const timeElapsed: any = endTime - startTime;
+
+    // console.log("timeElapsed: ", timeElapsed);
+    // console.log("okCount: ", okCount);
+
+    // var okBuffer = Buffer.from("+OK\n");
+
+    // client.on("data", function (data) {
+    //   // console.log(data.toString());
+
+    //   if (Buffer.compare(data, okBuffer) === 0) {
+    //     // console.log("ok");
+    //     return;
+    //   } else {
+    //     // console.log("err: ", data.toString());
+    //   }
+
+    //   // var is_kernel_buffer_full = client.write(data);
+    //   // if (!is_kernel_buffer_full) {
+    //   //   client.pause();
+    //   // }
+    //   // if (is_kernel_buffer_full) {
+    //   //   console.log(
+    //   //     "Data was flushed successfully from kernel buffer i.e written successfully!"
+    //   //   );
+    //   // } else {
+    //   //   client.pause();
+    //   // }
+    //   // receivedCount++;
+    //   // client.destroy(); // kill client after server's response
+    //   // console.log("Received: " + data);
+    //   // if (maxCount === receivedCount) {
+    //   //   console.log("YESSSSSSSSS");
+    //   // } else {
+    //   //   console.log(receivedCount);
+    //   // }
+    // });
+
+    // client.on("close", function () {
+    //   console.log("Connection closed");
+    // });
+
+    // client.on("timeout", function () {
+    //   console.log("Socket timed out !");
+    //   client.end("Timed out!");
+    //   // can call socket.destroy() here too.
+    // });
+
+    // client.on("drain", function () {
+    //   console.log(
+    //     "write buffer is empty now .. u can resume the writable stream"
+    //   );
+    //   client.resume();
+    // });
   }
 }
 
