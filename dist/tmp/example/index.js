5b43fcd8a9858c24e47142f4025efed6f3e48d7a
mac <mac@blockapi.dev>
2024-04-26 17:52:53 +0800

    0.2.4-dev

--- dist/tmp/example/index.js
+++ dist/tmp/example/index.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const index_1 = require("../index");
+const index_1 = require("../../src/index");
 console.log("@hatish/hati-sdk-node");
 (async () => {
     const hati = new index_1.HatiSdk({
